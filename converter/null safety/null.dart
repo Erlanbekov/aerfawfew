7dd0db910e8b0751a054d921007f805fc03cd277
Пользователь <abdyrazakov.e.s@gmail.com>
2022-01-19 19:00:21 +0600

    null

---
+++ converter/null safety/null.dart
@@ -0,0 +1,22 @@
+void main() {
+ 
+  forNull(null);
+ 
+  forNull1(null, null);
+ 
+}
+ 
+void forNull(String? name) {
+  print(name ?? 'Ошибка');
+}
+ 
+ 
+forNull1([int? a = 1, int? b = 2]) {
+  var c = b ?? 2;
+  var d = a ?? 1;
+ 
+  print(c + d);
+ 
+}
+ 
+
